b17644871fb4f63a1cf86ee6a813416c9e7b1273
Benjamin De Kosnik <bkoz@gnu.org>
2024-05-03 21:46:24 -0700

    update docs

--- docs/html/html.20230322.doxygen/search/typedefs_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['color_5farray_0',['color_array',['../namespacesvg.html#a2efb09518fa0cb4589768e71d7f6f75d',1,'svg']]],
-  ['color_5fqis_1',['color_qis',['../namespacesvg.html#a4ae14c5c1f7d83967579cc42e58232c8',1,'svg']]],
-  ['colorband_2',['colorband',['../namespacesvg.html#a92496507cd6630277fa95c5049673f12',1,'svg']]],
-  ['colormap_3',['colormap',['../structsvg_1_1render__state.html#a119f2e0f735f0a18293b90baa1a7064b',1,'svg::render_state']]],
-  ['colors_4',['colors',['../namespacesvg.html#a158ef3662755ee90c0191d15afebbd40',1,'svg']]]
-];
---
+++ docs/html/html.20240503.doxygen/search/all_5.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['e1s_0',['e1s',['../namespacesvg_1_1constants.html#a44821c901c5a38946ded48f013fa32b0a3dd816c2cfc9cc6c7ddbdb397cabdca6',1,'svg::constants']]],
+  ['e2s_1',['e2s',['../namespacesvg_1_1constants.html#a44821c901c5a38946ded48f013fa32b0aaf5a2ca4e4caabb31fcf908b4050a72d',1,'svg::constants']]],
+  ['e3s_2',['e3s',['../namespacesvg_1_1constants.html#a44821c901c5a38946ded48f013fa32b0a5fdcae1aa6bd7925e0261989b973ce58',1,'svg::constants']]],
+  ['e4s_3',['e4s',['../namespacesvg_1_1constants.html#a44821c901c5a38946ded48f013fa32b0add45a6bac352d3c810764b23d8c4baa0',1,'svg::constants']]],
+  ['e5s_4',['e5s',['../namespacesvg_1_1constants.html#a44821c901c5a38946ded48f013fa32b0a183189d62ddfc2b76f26003d3a999946',1,'svg::constants']]],
+  ['echo_5',['echo',['../namespacesvg_1_1constants.html#a39dbe47fdf4e5c28e2cbd2b2c0c56ceaacbb11ed87dc8a95d81400c7f33c7c171',1,'svg::constants']]],
+  ['element_5fbase_6',['element_base',['../group__elements.html#structsvg_1_1element__base',1,'svg']]],
+  ['element_5fto_5fsvg_5finsert_7',['element_to_svg_insert',['../namespacesvg.html#aea9d90bd4cb07cf304ab566f4e525ad3',1,'svg']]],
+  ['empty_8',['empty',['../group__elements.html#ad192becf9565fd5d15094d275e207481',1,'svg::element_base']]],
+  ['end_9',['end',['../structsvg_1_1typography.html#a93bca2e6c4e32784def555d27487f886a7f021a1415b86f2d013b2618fb31ae53',1,'svg::typography::end'],['../structsvg_1_1typography.html#ab466ab5a124304d826cea9c46a46d3b0a7f021a1415b86f2d013b2618fb31ae53',1,'svg::typography::end']]],
+  ['engineering_10',['engineering',['../namespacesvg.html#a0e622bba4eee0e6528a9261d6aa251d1a5d554bc5f3d2cd182cdd0952b1fb87ca',1,'svg']]],
+  ['even_11',['even',['../namespacesvg_1_1constants.html#a39dbe47fdf4e5c28e2cbd2b2c0c56ceaacc935c5faf4c8f7a0468d7552a9b8138',1,'svg::constants']]],
+  ['exitnode_12',['exitnode',['../namespacesvg_1_1constants.html#a39dbe47fdf4e5c28e2cbd2b2c0c56ceaa29bce8b0eee6268296ec8c151fefef2e',1,'svg::constants']]]
+];
