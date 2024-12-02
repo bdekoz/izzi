4b679abf6f92f07e6e9bc1f6b143406c2439aa9d
Benjamin De Kosnik <bdekosnik@mozilla.com>
2024-12-01 18:08:17 -0800

    simplify example

--- examples/video-1.cc
+++ examples/video-1.cc
@@ -1,38 +1,34 @@
  #include "a60-svg.h"
 
 void
-test_tri(std::string ofile)
+test_video(std::string ofile)
 {
   using namespace std;
   using namespace svg;
 
   svg_element obj(ofile, k::letter_096_v);
-  //  point_2t cp = obj.center_point();
+  point_2t cp = obj.center_point();
+  auto [ cpx, cpy ] = cp;
 
   const area<> a(270, 480);
-  rect_element::data rd = { 540, 960, 1080, 1920};
-     
+  rect_element::data rd = { cpx, cpy, 1080, 1920};
+
   foreign_element fe;
   fe.start_element(a, rd);
-  obj.add_element(fe);
 
   const string vsrc = "../docs/image/star-x-0-blink-0.2s.1080p.mkv";
-  video_element vb;
-  vb.start_element(a, vsrc, rd);
-  obj.add_element(vb);
+  video_element ve;
+  ve.start_element(a, vsrc, rd);
+  ve.finish_element();
+  fe.add_raw(ve.str());
 
-  video_element vf;
-  vf.finish_element();
-  obj.str(vf.str());
-
-  foreign_element fef;
-  fef.finish_element();
-  obj.str(fef.str());  
+  fe.finish_element();
+  obj.add_element(fe);
 }
 
 
 int main()
 {
-  test_tri("video-1");
+  test_video("video-1");
   return 0;
 }
