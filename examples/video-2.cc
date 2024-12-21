94c71a941215d2489db69bcdeb86900dee1938a8
Benjamin De Kosnik <bkoz@gnu.org>
2024-12-20 23:33:21 -0800

    update videos

--- examples/video-2.cc
+++ examples/video-2.cc
@@ -6,22 +6,24 @@ test_video(std::string ofile)
   using namespace std;
   using namespace svg;
 
-  const svg::area<> aa = { 1200, 900 };  
-  svg_element obj(ofile, aa);
+  const svg::area<> page = { 1920, 1080 };
+  svg_element obj(ofile, page);
   point_2t cp = obj.center_point();
   auto [ cpx, cpy ] = cp;
 
   // video in center
-  const area<> a(270, 480);
-  rect_element::data rd = { cpx - a._M_width/2, cpy - a._M_height/2,
-			    1080, 1920};
+  const string isrc="asama-0-oo-ee-oo-loop-5l.3.concat-all-ascii-white-1s.1080p-960x540.mp4";
+  const string vsrc = "../docs/image/" + isrc;
+  const area<> av(960, 540); // frame size of video file
+  const area<> arect(480, 260); // size of video in svg
+  rect_element::data rd = { cpx - av._M_width/2, cpy - av._M_height/2,
+			    av._M_width, av._M_height};
 
   foreign_element fe;
-  fe.start_element(a, rd);
+  fe.start_element(av, rd);
 
-  const string vsrc = "../docs/image/star-x-0-blink-0.2s.1080p.mkv";
   video_element ve;
-  ve.start_element(a, vsrc, rd);
+  ve.start_element(av, vsrc, rd);
   ve.finish_element();
   fe.add_raw(ve.str());
 
@@ -29,7 +31,8 @@ test_video(std::string ofile)
   obj.add_element(fe);
 
   // red dot at center
-  point_to_circle(obj, cp, svg::k::b_style, 4);  
+  const style rstyl = { color::red, 1.0, color::red, 0.0, 2 };
+  point_to_circle(obj, cp, rstyl, 4);
 }
 
 
