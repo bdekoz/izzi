59c597aed5064e80a2ec8a8b97289fa57d176746
Benjamin De Kosnik <bdekosnik@mozilla.com>
2025-03-22 19:30:02 -0700

    add wip line graph

---
+++ src/a60-svg-graphs-line.h
@@ -0,0 +1,60 @@
+// izzi line graphs -*- mode: C++ -*-
+
+// Copyright (c) 2025, Benjamin De Kosnik <b.dekosnik@gmail.com>
+
+// This file is part of the alpha60 library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software
+// Foundation; either version 3, or (at your option) any later
+// version.
+
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+
+#ifndef izzi_SVG_GRAPHS_LINE_H
+#define izzi_SVG_GRAPHS_LINE_H 1
+
+
+namespace svg {
+
+//// Distance between center and top/bottom block
+const double vspace(60);
+
+/// Distance between label and top/bottom of line or text
+const double vlspace(10);
+
+// Type sizes.
+const auto lsz = 16; // large bold
+const auto asz = 12; // sub headings
+const auto ssz = 10; // sub sub headings
+
+/**
+   Page Area
+   Graph Area
+
+   Input json
+
+   xaxis
+   yaxis
+   
+ */
+  
+
+// https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/  
+  
+// stroke_dash_array
+// Attribute/stroke-dasharray
+  
+// polyline marker-mid  
+
+
+// Attribute/marker-mid  
+
+
+//   https://www.highcharts.com/demo/highcharts/accessible-line
+  
+} // namepace svg
+
+#endif
