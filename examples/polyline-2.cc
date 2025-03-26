8919d30a883168ddbdbedae895cb81ff3a24c5a0
Benjamin De Kosnik <bkoz@gnu.org>
2025-03-25 22:46:49 -0700

    fix polyline-2 example with marker mid points

--- examples/polyline-2.cc
+++ examples/polyline-2.cc
@@ -11,12 +11,16 @@ const auto linelen = 150;
 }
 
 void
-make_marker_circle(const std::string id, const svg::area<> ma, const svg::point_2t mcp)
+make_marker_circle(const std::string id,
+		   const svg::area<> ma, const svg::point_2t mcp)
 {
   svg::marker_element mkr;
   mkr.start_element(id, ma, mcp);
 
-  svg::circle_element c = make_circle(cp, svg::k::b_style, 4);
+  using svg::color::red;
+  const svg::style r_style = { red, 1.0, red, 0.0, 0.5 };
+
+  svg::circle_element c = make_circle(mcp, r_style, 8);
   mkr.add_raw(c.str());
   mkr.finish_element();
   obj.add_element(mkr);
@@ -26,6 +30,7 @@ void
 make_markers()
 {
   make_marker_circle("c4", { 4, 4}, { 2, 2});
+  make_marker_circle("c8", { 8, 8}, { 4, 4});
 };
 
 void
@@ -49,7 +54,7 @@ test_polyline_1()
   const point_2t p7 { x + spacer * 6, y + spacer };
   const point_2t p8 { x + spacer * 7, y };
   vrange points { p1, p2, p3, p4, p5, p6, p7, p8 };
-  polyline_element pl = make_polyline(points, styl1, "2", "c4");
+  polyline_element pl = make_polyline(points, styl1, "", "c8");
   obj.add_element(pl);
 }
 
