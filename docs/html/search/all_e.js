38be02157137d51bf4c1292a28c57a8ac3e5fbcc
Benjamin De Kosnik <bkoz@gnu.org>
2025-06-01 20:40:57 -0700

    update docs

--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -4,11 +4,12 @@ var searchData=
   ['narrow_5fvspace_1',['narrow_vspace',['../namespacesvg.html#a146854def101fda6cf807910ae725526',1,'svg']]],
   ['navajowhite_2',['navajowhite',['../namespacesvg.html#a0e622bba4eee0e6528a9261d6aa251d1a4f66b601b5bad722855550fb6d0175d3',1,'svg']]],
   ['navy_3',['navy',['../namespacesvg.html#a0e622bba4eee0e6528a9261d6aa251d1af3311d3d14c8437553bf3e3f90920736',1,'svg']]],
-  ['newline_4',['newline',['../namespacesvg_1_1constants.html#aa7f04c2ea699a1b1e2e790de5ac33f38',1,'svg::constants']]],
-  ['next_5fcolor_5',['next_color',['../namespacesvg.html#a554dc780ece47a3937550a8bd113b604',1,'svg']]],
-  ['next_5fin_5fcolor_5fband_6',['next_in_color_band',['../namespacesvg.html#ae25614aaac6374dd8f78d382dfaa4fd2',1,'svg']]],
-  ['no_5farea_7',['no_area',['../namespacesvg_1_1constants.html#a7ea99d84ad1fa1bcbeb32c206b43c19b',1,'svg::constants']]],
-  ['no_5fstyle_8',['no_style',['../namespacesvg_1_1constants.html#a94e31e0f92ab672062d8ebb87e2c8975',1,'svg::constants']]],
-  ['none_9',['none',['../structsvg_1_1typography.html#a13d320c2ae85f1d4f3ea33806e60a7b9a334c4a4c42fdb79d7ebc3e73b517e6f8',1,'svg::typography::none'],['../namespacesvg.html#a0e622bba4eee0e6528a9261d6aa251d1a334c4a4c42fdb79d7ebc3e73b517e6f8',1,'svg::none'],['../namespacesvg.html#a5ab95bf715f4785805c5aaaf74d97579a334c4a4c42fdb79d7ebc3e73b517e6f8',1,'svg::none']]],
-  ['normal_10',['normal',['../structsvg_1_1typography.html#ac41cf6a133f5b21757130ce5ba72b3d7afea087517c26fadd409bd4b9dc642555',1,'svg::typography::normal'],['../structsvg_1_1typography.html#aba5bf3a1484966319aebc9443746e98cafea087517c26fadd409bd4b9dc642555',1,'svg::typography::normal']]]
+  ['nest_5finner_5fsvg_5felement_5fcentered_4',['nest_inner_svg_element_centered',['../namespacesvg.html#aa07a5304f7966198fab897e7b8ac98fb',1,'svg']]],
+  ['newline_5',['newline',['../namespacesvg_1_1constants.html#aa7f04c2ea699a1b1e2e790de5ac33f38',1,'svg::constants']]],
+  ['next_5fcolor_6',['next_color',['../namespacesvg.html#a554dc780ece47a3937550a8bd113b604',1,'svg']]],
+  ['next_5fin_5fcolor_5fband_7',['next_in_color_band',['../namespacesvg.html#ae25614aaac6374dd8f78d382dfaa4fd2',1,'svg']]],
+  ['no_5farea_8',['no_area',['../namespacesvg_1_1constants.html#a7ea99d84ad1fa1bcbeb32c206b43c19b',1,'svg::constants']]],
+  ['no_5fstyle_9',['no_style',['../namespacesvg_1_1constants.html#a94e31e0f92ab672062d8ebb87e2c8975',1,'svg::constants']]],
+  ['none_10',['none',['../structsvg_1_1typography.html#a13d320c2ae85f1d4f3ea33806e60a7b9a334c4a4c42fdb79d7ebc3e73b517e6f8',1,'svg::typography::none'],['../namespacesvg.html#a0e622bba4eee0e6528a9261d6aa251d1a334c4a4c42fdb79d7ebc3e73b517e6f8',1,'svg::none'],['../namespacesvg.html#a5ab95bf715f4785805c5aaaf74d97579a334c4a4c42fdb79d7ebc3e73b517e6f8',1,'svg::none']]],
+  ['normal_11',['normal',['../structsvg_1_1typography.html#ac41cf6a133f5b21757130ce5ba72b3d7afea087517c26fadd409bd4b9dc642555',1,'svg::typography::normal'],['../structsvg_1_1typography.html#aba5bf3a1484966319aebc9443746e98cafea087517c26fadd409bd4b9dc642555',1,'svg::typography::normal']]]
 ];
