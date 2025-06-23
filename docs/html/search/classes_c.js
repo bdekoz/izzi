57dd563a76845f720075dabbce8234e292b143cb
Benjamin De Kosnik <bkoz@gnu.org>
2025-06-22 23:04:02 -0700

    update docs

--- docs/html/search/classes_c.js
+++ docs/html/search/classes_c.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['string_0',['string',['../classsvg_1_1string.html',1,'svg']]],
-  ['stroke_5fstyle_1',['stroke_style',['../structsvg_1_1stroke__style.html',1,'svg']]],
-  ['style_2',['style',['../structsvg_1_1style.html',1,'svg']]],
-  ['svg_5felement_3',['svg_element',['../group__elements.html#structsvg_1_1svg__element',1,'svg']]]
+  ['script_5felement_0',['script_element',['../group__elements.html#structsvg_1_1script__element',1,'svg']]],
+  ['string_1',['string',['../classsvg_1_1string.html',1,'svg']]],
+  ['stroke_5fstyle_2',['stroke_style',['../structsvg_1_1stroke__style.html',1,'svg']]],
+  ['style_3',['style',['../structsvg_1_1style.html',1,'svg']]],
+  ['svg_5felement_4',['svg_element',['../group__elements.html#structsvg_1_1svg__element',1,'svg']]]
 ];
