a2b6eee2f1fa5b08e1c1e7fa331b86ddd2dfd4ea
Benjamin De Kosnik <bkoz@gnu.org>
2025-07-13 20:59:41 -0700

    update docs

--- docs/html/search/functions_b.js
+++ docs/html/search/functions_b.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['narrow_5fvspace_0',['narrow_vspace',['../namespacesvg.html#a146854def101fda6cf807910ae725526',1,'svg']]],
-  ['nest_5finner_5fsvg_5felement_5fcentered_1',['nest_inner_svg_element_centered',['../namespacesvg.html#aa07a5304f7966198fab897e7b8ac98fb',1,'svg']]],
-  ['newline_2',['newline',['../namespacesvg_1_1constants.html#aa7f04c2ea699a1b1e2e790de5ac33f38',1,'svg::constants']]],
-  ['next_5fcolor_3',['next_color',['../namespacesvg.html#a554dc780ece47a3937550a8bd113b604',1,'svg']]],
-  ['next_5fin_5fcolor_5fband_4',['next_in_color_band',['../namespacesvg.html#ae25614aaac6374dd8f78d382dfaa4fd2',1,'svg']]]
+  ['nest_5finner_5felement_1',['nest_inner_element',['../namespacesvg.html#a14179ddf3ee33f17012b21b473bcc827',1,'svg']]],
+  ['nest_5finner_5fsvg_5felement_5fcentered_2',['nest_inner_svg_element_centered',['../namespacesvg.html#aa07a5304f7966198fab897e7b8ac98fb',1,'svg']]],
+  ['newline_3',['newline',['../namespacesvg_1_1constants.html#aa7f04c2ea699a1b1e2e790de5ac33f38',1,'svg::constants']]],
+  ['next_5fcolor_4',['next_color',['../namespacesvg.html#a554dc780ece47a3937550a8bd113b604',1,'svg']]],
+  ['next_5fin_5fcolor_5fband_5',['next_in_color_band',['../namespacesvg.html#ae25614aaac6374dd8f78d382dfaa4fd2',1,'svg']]]
 ];
